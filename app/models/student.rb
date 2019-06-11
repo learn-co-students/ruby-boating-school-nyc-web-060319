bbb9944cda3676cd4aae27396e1efcc93d498c77
TimeSmash <TimeSmash@github.com>
2019-06-11 10:08:55 -0400

    All tests passing, performed with seed data in console.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,57 @@
+# `Student` class:
+
+# Y should initialize with `first_name`
+
+# Y `Student.all` should return all of the student instances
+
+# Y `Student#add_boating_test` should initialize a new boating test with 
+#    a Student (Object), a boating test name (String), a boating test status (String), 
+#    and an Instructor (Object)
+
+# Y `Student.find_student` will take in a first name and output
+#    the student (Object) with that name
+
+# Y `Student#grade_percentage` should return the percentage of tests 
+#    that the student has passed, a Float 
+#    (so if a student has passed 3 / 9 tests that they've taken, this method should return 
+#    the Float `33.33`)
+
+
 class Student
+
+    attr_reader :first_name
+
+    attr_accessor :boating_test
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_student(first_name)
+        self.all.find { |student| student.first_name == first_name }
+    end
+
+    def add_boating_test(test_name, test_status)
+        new_b_test = BoatingTest.new(self, test_name, test_status)
+    end
+
+    def my_tests
+        BoatingTest.all.select{ |test| test.student == self }
+    end
+
+    def passing_tests
+        my_tests.select { |my_test| my_test.test_status == "Pass"}
+    end
+
+    def grade_percentage #returns percentage of tests stuent
+        (self.passing_tests.length.to_f / self. my_tests.length.to_f).round(2)
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,26 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+toad = Student.new("Toad")
+peach = Student.new("Peach")
+daisy = Student.new("Daisy")
+
+toad_jetski = toad.add_boating_test("Jetski", "Pass")
+toad_canoe =  toad.add_boating_test("Canoe", "Fail")
+toad_pontoon = toad.add_boating_test("Pontoon", "Pass")
+
+peach_paddleboat = peach.add_boating_test("Goose Paddleboat", "Pass")
+peach_canoe = peach.add_boating_test("Canoe", "Fail")
+peach_waterbike = peach.add_boating_test("Waterbike", "Pass")
+
+daisy_canoe = daisy.add_boating_test("Canoe", "Pass")
+
+wario_instructor = Instructor.new("Wario")
+waluigi_instructor =  Instructor.new("Waluigi")
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
